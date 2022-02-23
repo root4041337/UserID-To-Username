930102a334fb9c4f03ee6378389f9bf4898cd268
root-404 <59521514+root4041337@users.noreply.github.com>
2022-02-23 14:00:33 +0100

    Add files via upload

---
+++ UserID to Username/Extensions/config.cs
@@ -0,0 +1,10 @@
+﻿namespace UserID_to_Username.Extensions
+{
+    public class config
+    {
+        public const string URL = "https://discord.com/api/v8/users/";
+        public const string URL2 = "/profile?with_mutual_guilds=false";
+
+        public const string AppVersion = "v0.1";
+    }
+}
